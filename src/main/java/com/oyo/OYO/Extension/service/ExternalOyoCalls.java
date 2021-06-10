62e3cc119fca1fc70b9beb11abdb93021a03f4e0
Nikunj Labana <user@OYOs-MacBook-Air.local>
2021-06-10 19:43:47 +0530

    create order OYO

--- src/main/java/com/oyo/OYO/Extension/service/ExternalOyoCalls.java
+++ src/main/java/com/oyo/OYO/Extension/service/ExternalOyoCalls.java
@@ -6,8 +6,15 @@ import com.oyo.OYO.Extension.model.Product;
 import com.oyo.OYO.Extension.model.ProductDetails;
 import com.oyo.OYO.Extension.model.ProductsOYO;
 import com.oyo.OYO.Extension.model.Transaction;
+import com.oyo.OYO.Extension.model.TransactionDetails;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,36 +22,46 @@ import java.util.List;
 @Component
 public class ExternalOyoCalls {
 
-    @Value("${OYO.base_url}")
-    private String oyobaseurl;
+  @Value("${OYO.base_url}")
+  private String oyobaseurl;
 
-    @Value("${create.order.path}")
-    private String createOrderPath;
+  @Value("${create.order.path}")
+  private String createOrderPath;
 
 
-    public void CreateOrder(Transaction transaction, ProductDetails productDetails) {
-        OrderRequestOYO orderRequestOYO = CreateOrderRequest(transaction, productDetails);
-
+  public void CreateOrder(Transaction transaction, ProductDetails productDetails) {
+    OrderRequestOYO orderRequestOYO = CreateOrderRequest(transaction, productDetails);
+    String baseUrl = oyobaseurl + createOrderPath;
+    HttpEntity<?> requestEntity = new HttpEntity<>(orderRequestOYO);
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<String> responseEntity;
+    try {
+      responseEntity = restTemplate.exchange(baseUrl, HttpMethod.POST, requestEntity, String.class);
+    } catch (RestClientException e) {
 
     }
 
-    private OrderRequestOYO CreateOrderRequest(Transaction transaction, ProductDetails productDetails) {
-        OrderRequestOYO orderRequestOYO = new OrderRequestOYO();
-        List<ProductsOYO> productsOYOList = new ArrayList<>();
-        for (Product product : productDetails.getResponse()) {
-            ProductsOYO productsOYO = new ProductsOYO();
-            Double amount = Double.parseDouble(product.getProduct_sum())/ Currency.INR.getMultiplicationFactor();
-            productsOYO.setAmount(amount);
-            productsOYO.setDescription(product.getProduct_name());
-            productsOYO.setName(product.getProduct_name());
-            productsOYOList.add(productsOYO);
-        }
-        orderRequestOYO.setProducts(productsOYOList);
-        orderRequestOYO.setBookingId(transaction.getTransaction_comment());
-        orderRequestOYO.setUserProfileId(transaction.getTransaction_comment());
-        orderRequestOYO.setCurrency(Currency.INR);
-        orderRequestOYO.setIsPaid(Boolean.FALSE);
-        orderRequestOYO.setCategory("FOOD");
-        return orderRequestOYO;
+  }
+
+  private OrderRequestOYO CreateOrderRequest(Transaction transaction,
+      ProductDetails productDetails) {
+    OrderRequestOYO orderRequestOYO = new OrderRequestOYO();
+    List<ProductsOYO> productsOYOList = new ArrayList<>();
+    for (Product product : productDetails.getResponse()) {
+      ProductsOYO productsOYO = new ProductsOYO();
+      Double amount =
+          Double.parseDouble(product.getProduct_sum()) / Currency.INR.getMultiplicationFactor();
+      productsOYO.setAmount(amount);
+      productsOYO.setDescription(product.getProduct_name());
+      productsOYO.setName(product.getProduct_name());
+      productsOYOList.add(productsOYO);
     }
+    orderRequestOYO.setProducts(productsOYOList);
+    orderRequestOYO.setBookingId(transaction.getTransaction_comment());
+    orderRequestOYO.setUserProfileId(transaction.getTransaction_comment());
+    orderRequestOYO.setCurrency(Currency.INR);
+    orderRequestOYO.setIsPaid(Boolean.FALSE);
+    orderRequestOYO.setCategory("FOOD");
+    return orderRequestOYO;
+  }
 }
